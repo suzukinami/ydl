ab9398095ae047190c39e3f591269a7e9c2223af
鈴木奈美 <166300634+suzukinami@users.noreply.github.com>
2024-04-13 17:57:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,34 @@ from subprocess import run
 
 app = Flask(__name__)
 
-ytdlp = 'https://apis.caymankun.f5.si/cgi-bin/yt-dlp'
+# ファイルをダウンロードして指定されたパスに保存する関数
+def download_file(url, save_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        return True
+    else:
+        return False
+
+# yt-dlpをダウンロードして実行
+yt_dlp_url = "https://apis.caymankun.f5.si/cgi-bin/yt-dlp"
+yt_dlp_path = "/tmp/yt-dlp"
+if download_file(yt_dlp_url, yt_dlp_path):
+    os.chmod(yt_dlp_path, 0o755)  # ダウンロードしたファイルを実行可能にする
+
+# ffmpegをダウンロードして実行
+ffmpeg_url = "https://apis.caymankun.f5.si/cgi-bin/ffmpeg"
+ffmpeg_path = "/tmp/ffmpeg"
+if download_file(ffmpeg_url, ffmpeg_path):
+    os.chmod(ffmpeg_path, 0o755)  # ダウンロードしたファイルを実行可能にする
 
 @app.route('/v', methods=['GET'])
 def download_video():
     video_id = request.args.get('id')
     if video_id:
         output_path = f"/tmp/%(title)s-%(id)s.%(ext)s"
-        run([ytdlp, "--ffmpeg-location" , "https://apis.caymankun.f5.si/cgi-bin/ffmpeg" , "-o", output_path, f"https://www.youtube.com/watch?v={video_id}"])
+        run([yt_dlp_path, "--ffmpeg-location" , ffmpeg_path , "-o", output_path, f"https://www.youtube.com/watch?v={video_id}"])
         if os.path.exists(output_path):
             mime_type, _ = mimetypes.guess_type(output_path)
             return send_file(output_path, mimetype=mime_type, as_attachment=True)
@@ -26,7 +46,7 @@ def download_audio():
     video_id = request.args.get('id')
     if video_id:
         output_path = f"/tmp/%(title)s-%(id)s.%(ext)s"
-        run([ytdlp, "-x", "--audio-format", "mp3", "--ffmpeg-location" , "https://apis.caymankun.f5.si/cgi-bin/ffmpeg" , "-o", output_path, f"https://www.youtube.com/watch?v={video_id}"])
+        run([yt_dlp_path, "-x", "--audio-format", "mp3", "--ffmpeg-location" , ffmpeg_path , "-o", output_path, f"https://www.youtube.com/watch?v={video_id}"])
         if os.path.exists(output_path):
             mime_type, _ = mimetypes.guess_type(output_path)
             return send_file(output_path, mimetype=mime_type, as_attachment=True)
