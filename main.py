5f2330231dd05dc53e54050689413f2cb6eb947a
鈴木奈美 <166300634+suzukinami@users.noreply.github.com>
2024-04-14 16:57:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ def download_video():
     url = request.args.get('url')
     if url:
         ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'outtmpl': '/tmp/%(title)s-%(id)s.%(ext)s',
+            'f': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'o': '/tmp/%(title)s-[%(id)s].%(ext)s',
+            'ffmpeg-location': 'ffmpeg',
+            
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
@@ -42,22 +44,20 @@ def download_video():
     else:
         return 'URL parameter is required', 400
 
-@app.route('/a', methods=['GET'])
-def download_audio():
+@app.route('/v', methods=['GET'])
+def download_video():
     url = request.args.get('url')
     if url:
         ydl_opts = {
-            'format': 'bestaudio',
-            'outtmpl': '/tmp/%(title)s-%(id)s.%(ext)s',
+            'f': 'bestaudio[ext=m4a]',
+            'o': '/tmp/%(title)s-[%(id)s].%(ext)s',
+            'ffmpeg-location': '/tmp/ffmpeg',
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            # ダウンロードした音声のパスを取得
-            audio_path = ydl.prepare_filename(info)
-            # 音声をMP3形式に変換
-            output_path = audio_path.replace('.*', '.mp3')
-            ffmpeg.input(audio_path).output(output_path).run()
-            return send_file(output_path, as_attachment=True)
+            # ダウンロードした動画のパスを取得
+            video_path = ydl.prepare_filename(info)
+            return send_file(video_path, as_attachment=True)
     else:
         return 'URL parameter is required', 400
 
